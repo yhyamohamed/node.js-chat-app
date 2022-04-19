f9604a57fca708adc18b08d4a2e0fd6b3010d7b5
yhyamohamed <62445980+yhyamohamed@users.noreply.github.com>
2022-04-19 22:02:50 +0200

    chat app v1

--- app.js
+++ app.js
@@ -1,37 +1,113 @@
 const express = require("express");
 const app = express();
-const http = require('http');
-const socketio = require('socket.io');
+const http = require("http");
+const socketio = require("socket.io");
 const server = http.createServer(app);
 const io = socketio(server);
 const path = require("path");
+const moment = require('moment')
+const { faker } = require("@faker-js/faker");
+const fs = require("fs");
 
-const port = process.env.port ||3000;
-
-app.use(express.json())
+const port = process.env.port || 3000;
+// ============================================
+app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname + "/public")));
-
-app.get('/',(req,res,next)=>{
-    res.render('home');
-})
-io.on('connection',socket=>{
-
-    //for only the sender 
-    socket.emit('message','welcom bud')
-    ///for all except sender 
-    socket.broadcast.emit();
-    //for every body
-    io.emit();
-    //when close 
-    socket.on('disconnect',()=>{
-        io.emit('message','a user disconnected');
-    })
-})
-app.use((err,req,res,next)=>{
-    res.status(402).send({error:err.message})
-})
-server.listen(port, () => {
-    console.log(`app started on port ${port}`);
+// ============================================
+app.get("/", (req, res, next) => {
+  res.render("home");
+});
+app.get("/file", (req, res, next) => {
+  obj = fs.readFileSync("./data.json", (err, data) => {
+    if (err) console.log(err);
   });
-  
+  obj = JSON.parse(obj);
+  res.send(obj);
+});
+app.get("/file", (req, res, next) => {
+  obj = fs.readFileSync("./data.json", (err, data) => {
+    if (err) console.log(err);
+  });
+  obj = JSON.parse(obj);
+  res.send(obj);
+});
+// ============================================
+io.on("connection", (socket) => {
+  let user = createUser(socket.id);
+  //   console.log(socket.id);
+  //for only the sender
+  socket.emit("online-msg",  "you are online");
+
+  ///for all except sender
+  socket.broadcast.emit("online-msg","a user jsut enterd the chat") ;
+
+  //for every body
+  socket.on("send-msg", (message) => {
+      message['time'] = moment().format('h:mm a');
+    io.emit("message", message);
+  });
+
+  //when close
+  socket.on("disconnect", () => {
+    io.emit("offline-msg", "a user disconnected");
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(402).send({ error: err.message });
+});
+server.listen(port, () => {
+  console.log(`app started on port ${port}`);
+});
+// ============================================
+// ============================================
+const constructMesg = (userObj, text) => {
+  let msgobj = {
+    sender: userObj.userName,
+    sender_id: userObj.user_id,
+    body: text,
+  };
+//   console.log('sender',msgobj);
+  return msgobj;
+};
+const createUser = (socketId) => {
+  let user = {
+    userName: faker.name.firstName(),
+    user_id: socketId,
+  };
+  console.log('user',user);
+  ;
+  return addUser(user);
+};
+// const checkUser = (socketId) => {
+//     let user= null;
+//     if (fs.existsSync("./data.json")) {
+//        let obj = fs.readFileSync("./data.json", (err, data) => {
+//           if (err) console.log(err);
+//         });
+//         obj = JSON.parse(obj);
+//         user = obj.filter((ele) => ele.user_id == socketId);
+//       }
+//     console.log('user',user)
+//     // addUser(user)
+//   };
+const addUser = (userObj) => {
+  let obj = null;
+  if (fs.existsSync("./data.json")) {
+    obj = fs.readFileSync("./data.json", (err, data) => {
+      if (err) console.log(err);
+    });
+  }
+  if (obj) {
+    obj = JSON.parse(obj);
+    obj.push(userObj);
+  } else {
+    obj = [];
+    obj.push(userObj);
+  }
+  fs.writeFile("./data.json", JSON.stringify(obj), (err) => {
+    if (err) console.log(err);
+  });
+  return userObj;
+};
--- public/scripts.js
+++ public/scripts.js
@@ -1,21 +1,99 @@
-const chatArea = $('#chat-area');
-const inputArea = $('#input-msg');
-const sendBtn = $('#send-btn');
-const chatForm = $('#chat-form');
-const socket =  io();
+const chatArea = $("#chat-area");
+const inputArea = $("#input-msg");
+const sendBtn = $("#send-btn");
+const chatForm = $("#chat-form");
+const messageBox = $("#message-group");
+let users;
+let user = "unknown";
+let s_id;
+const socket = io();
+//get users data
+// jQuery.ajaxSetup({
+//     cache: false
+// });
+const getAllUsers = () => {
+  let users;
+  $.ajax({
+    url: "/file",
+    success: (res) => {
+      users = res;
+      getUser(s_id, users);
+    },
+    error: (err) => {
+      errors = err;
+    },
+  });
+};
+///////////////////////
 
-socket.on('message',message=>{
-    console.log(message)
+socket.on("connect", () => {
+  //   user = getFileData(socket.id);
+  s_id = socket.id;
+  getFileData();
+});
+socket.on("message", (message) => {
+  appendMessage(message);
+  chatArea.scrollTop(chatArea.prop("scrollHeight"));
+});
+socket.on("online-msg", (message) => {
+//   console.log("msg", message);
+//   user = { userName: message.sender, user_id: message.sender_id };
+//   console.log("user", user);
+  chatArea.append(welcomMessageStructure(message));
+  chatArea.scrollTop(chatArea.prop("scrollHeight"));
 });
 
-
-chatForm.submit( (e)=> { 
-    e.preventDefault();
-    let chat_msg = inputArea.val()
-    inputArea.empty()
-    console.log(chat_msg) 
+socket.on("offline-msg", (message) => {
+  chatArea.append(disconnectMessageStructure(message));
+  chatArea.scrollTop(chatArea.prop("scrollHeight"));
 });
 
-// inputArea.focus(()=>{
-//     inputArea.attr('placeholder')
-// })
+chatForm.submit((e) => {
+  e.preventDefault();
+  let chat_msg = inputArea.val();
+//   message = user;
+let message ={};
+  message["body"] = chat_msg;
+  inputArea.val("");
+  socket.emit("send-msg", message);
+});
+const getFileData = () => {
+  getAllUsers();
+};
+const getUser = (id, users) => {
+  user = users.filter((ele) => ele.user_id == id);
+  if (user.length > 0) user = user[0];
+  return user;
+};
+function appendMessage(message) {
+//   console.log("m", message);
+  chatArea.append(messageStructure(message));
+}
+const findSender = (id) => {
+    console.log(id,s_id)
+    console.log(users)
+};
+const messageStructure = (msg) => {
+  return ` <div class="mt-1 d-flex flex-column justify-content-end align-items-end "message-group">
+            <div class="alert alert-info p-1  mb-0" role="alert" >
+                ${msg.body}
+            </div>
+            <span class="badge bg-light text-dark  ">${msg.time} | today</span>
+        </div>`;
+};
+const welcomMessageStructure = (msg) => {
+  return ` <div class=" d-flex flex-column justify-content-center align-items-center message-group">
+            <div class="w-100 alert alert-dark p-1  mb-0" role="alert" >
+                ${msg}
+            </div>
+        </div>`;
+};
+
+const disconnectMessageStructure = (msg) => {
+  return ` <div class=" d-flex flex-column justify-content-center align-items-center message-group">
+            <div class="w-100 alert alert-warning p-1  mb-0" role="alert" >
+                ${msg}
+            </div>
+        </div>`;
+};
+
